46c44ffe502b3ff3f109ce670f349791bd59462c
WebCoder346 <sauravcoder1@gmail.com>
2024-08-14 13:16:36 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,165 +1,147 @@
 const defaultSong = {
-  image: "/SpotifyClone/img/img10.jpeg",
-  song: "/SpotifyClone/songs/song8.mp3",
+  image: "img10.jpeg",
+  song: "song8.mp3",
   title: "Aam Jahe Munde",
   artist: "Parmish Verma",
 }
 const data = [
   {
-    image: "/SpotifyClone/img/img3.jpeg",
-    song: "/SpotifyClone/songs/song1.mp3",
+    image: "img3.jpeg",
+    song: "song1.mp3",
     title: "Besabriyaan",
     artist: "Armaan Malik",
     type: ["msd"],
   },
   {
-    image: "/SpotifyClone/img/img3.jpeg",
-    song: "/SpotifyClone/songs/song2.mp3",
+    image: "img3.jpeg",
+    song: "song2.mp3",
     title: "Jab Tak",
     artist: "Armaan Malik",
     type: ["msd"],
-
   },
   {
-    image: "/SpotifyClone/img/img3.jpeg",
-    song: "/SpotifyClone/songs/song3.mp3",
+    image: "img3.jpeg",
+    song: "song3.mp3",
     title: "Kaun Tujhe",
     artist: "Palak Muchhal",
     type: ["msd"],
-
   },
   {
-    image: "/SpotifyClone/img/img1.jpeg",
-    song: "/SpotifyClone/songs/song4.mp3",
+    image: "img1.jpeg",
+    song: "song4.mp3",
     title: "Pahle Bhi Mai",
     artist: "Paarmish Verma, PARDHAAN, Sadhana Sargam",
     type: ["love"],
-
   },
   {
-    image: "/SpotifyClone/img/img3.jpeg",
-    song: "/SpotifyClone/songs/song5.mp3",
+    image: "img3.jpeg",
+    song: "song5.mp3",
     title: "Phir Kabhi",
     artist: "Arijit Singh",
     type: ["msd"],
-
   },
-
-
-
   {
-    image: "/SpotifyClone/img/img1.jpeg",
-    song: "/SpotifyClone/songs/song6.mp3",
+    image: "img1.jpeg",
+    song: "song6.mp3",
     title: "Saari Duniya Jalaa Denga",
     artist: "Jaani, B Araak",
     type: ["arijit", "love"],
-
   },
   {
-    image: "/SpotifyClone/img/img2.jpeg",
-    song: "/SpotifyClone/songs/song7.mp3",
+    image: "img2.jpeg",
+    song: "song7.mp3",
     title: "Hasi-Male version",
     artist: "Ami Mishra",
     type: ["love", "arijit"],
-
   },
   {
-    image: "/SpotifyClone/img/img10.jpeg",
-    song: "/SpotifyClone/songs/song8.mp3",
+    image: "img10.jpeg",
+    song: "song8.mp3",
     title: "Aam Jahe Munde",
     artist: "Parmish Verma",
     type: ["punjabi"],
-
   },
   {
-    image: "/SpotifyClone/img/img9.jpeg",
-    song: "/SpotifyClone/songs/song9.mp3",
+    image: "img9.jpeg",
+    song: "song9.mp3",
     title: "Daku",
     artist: "Chani Nattan,Shidu",
     type: ["punjabi"],
-
   },
   {
-    image: "/SpotifyClone/img/img11.jpeg",
-    song: "/SpotifyClone/songs/song10.mp3",
+    image: "img11.jpeg",
+    song: "song10.mp3",
     title: "Ve Kamleya-Lofi",
     artist: "Deepanshu, Arijit Singh",
     type: ["lofi", "arijit"],
-
   },
   {
-    image: "/SpotifyClone/img/img4.jpeg",
-    song: "/SpotifyClone/songs/song11.mp3",
+    image: "img4.jpeg",
+    song: "song11.mp3",
     title: "Phir Bhi Tumko Chaahuga",
     artist: "Arijit Singh",
     type: ["arijit", "lofi"],
-
   },
   {
-    image: "/SpotifyClone/img/img6.jpeg",
-    song: "/SpotifyClone/songs/song12.mp3",
+    image: "img6.jpeg",
+    song: "song12.mp3",
     title: "Asal Mein",
     artist: "Darshan Raval",
     type: ["love"],
-
   },
   {
-    image: "/SpotifyClone/img/img7.jpeg",
-    song: "/SpotifyClone/songs/song13.mp3",
+    image: "img7.jpeg",
+    song: "song13.mp3",
     title: "Tere Sang Yarra",
     artist: "Atif Aslam, Manoj Muntashir",
     type: ["arijit", "lofi", "love"],
-
   },
-
-
-
 ]
 
 const playlist = [
   {
-    image: "/SpotifyClone/img/img5.jpeg",
+    image: "img5.jpeg",
     title: "Lofi Songs (sukoon vibes)💜",
     type: "lofi",
   },
   {
-    image: "/SpotifyClone/img/img3.jpeg",
+    image: "img3.jpeg",
     title: "Ms. Dhoni All Songs",
     type: "msd",
   },
   {
-    image: "/SpotifyClone/img/img12.jpeg",
+    image: "img12.jpeg",
     title: "PUNJABI SONGS",
     type: "punjabi",
   },
   {
-    image: "/SpotifyClone/img/img4.jpeg",
+    image: "img4.jpeg",
     title: "The Arijit Singh Cool",
     type: "arijit"
-  }
+  },
 ]
 const today = [
   {
-    image: "/SpotifyClone/img/img13.jpeg",
+    image: "img13.jpeg",
     title: "Bhakti (santi vibes)"
   },
 ]
 
 const backInData = [
   {
-    image: "/SpotifyClone/img/img8.jpeg",
+    image: "img8.jpeg",
     title: "Arijit Singh Mix",
   },
   {
-    image: "/SpotifyClone/img/img10.jpeg",
+    image: "img10.jpeg",
     title: "Punjabi Hits",
   },
   {
-    image: "/SpotifyClone/img/img5.jpeg",
+    image: "img5.jpeg",
     title: "Relax-Lofi",
   },
   {
-    image: "/SpotifyClone/img/img6.jpeg",
+    image: "img6.jpeg",
     title: "Chill-Love Songs",
-  }
+  },
 ]
